3e795129b87abc21e79304d9401725ec6917b4e6
gabrielebottari <gabriele.bottari1@gmail.com>
2023-12-05 14:10:14 +0100

    ADD bonus 1

--- js/scripts.js
+++ js/scripts.js
@@ -21,30 +21,45 @@ Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot f
 
 */
 
-/*
-Scrivi un programma che stampi in console i numeri da 1 a 100,
-ma che per i multipli di 3 stampi "Fizz" al posto del numero e per i multipli di 5 stampi "Buzz".
-Per i numeri che sono sia multipli di 3 che di 5 stampi "FizzBuzz".
-*/
+//div container nel DOM
+const container = document.createElement("div");
+
+// Aggiungo il container al corpo del documento
+document.body.append(container);
 
 // ciclo for con numeri da 1 a 100 con avanzamento di 1
 for (let number = 1; number <= 100; number++) {
 
+    //div elemento creato
+    const element = document.createElement('div');
+    console.log('element', element, typeof element);
+
     //se divisibile per 3 e 5 dando resto 0 come prima condizione stampa fizzbuzz
     if (number % 3 == 0 && number % 5 == 0) {
         console.log("FizzBuzz");
+        //inserimento nell'elemento
+        element.append("FizzBuzz"); 
     
     //se divisibile per 3 dando resto 0 stampa fizz
     } else if (number % 3 == 0) {
         console.log("Fizz");
+        //inserimento nell'elemento
+        element.append("Fizz");
 
     //se divisibile per 5 dando resto 0 stampa buzz
     } else if (number % 5 == 0) {
         console.log("Buzz");
+        //inserimento nell'elemento
+        element.append("Buzz"); //inserimento nell'elemento
 
     //altrimenti stampa in numero
     } else {
         console.log(number);
+        //inserimento nell'elemento
+        element.append(number)
     }
+
+    //inserimento elemento nel container
+    container.append(element);
 
 }
